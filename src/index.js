1617fd3d8e0adef5cf7b67336e982077bdd165d5
Diego Ocaranza <diego.ocaranza@outlook.com>
2021-11-26 18:18:05 -0300

    otra nueva correccion al maldito font awesome

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/font-awesome/css/font-awesome.min.css';
+// import '../node_modules/font-awesome/css/font-awesome.min.css';
+import '../node_modules/fontawesome-4.7/css/font-awesome.min.css';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import store from './redux/store'
